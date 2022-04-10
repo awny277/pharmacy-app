2992309c67714b1fcd2bf22b4a5a3d29ad7f1c58
awny277 <at.awny@yahoo.com>
2022-04-10 13:05:52 +0200

    update

--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -4,6 +4,7 @@ import { BsTelephoneInbound } from "react-icons/bs";
 import { HiOutlineMail } from "react-icons/hi";
 import "./Contact.css";
 import emailjs from "@emailjs/browser";
+import { Col, Row } from "react-bootstrap";
 const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
@@ -27,54 +28,68 @@ const Contact = () => {
   };
   return (
     <div className="Touch" id="featuer">
-      <div className="container">
+      <div className="container-fluid">
         <div className="Team-Header text-center">
           <span>ابقى على تواصل</span>
           <h1>تواصل معنا لأي استفسار</h1>
         </div>
         <div className="Touch-contant">
-          <div className="touch-information">
-            <div className="touch-left">
-              <div className="touch-icon">
-                <GoLocation />
+          <Row className="touch-row">
+            <Col md={5}>
+              <div className="touch-information">
+                <div className="touch-left">
+                  <div className="touch-icon">
+                    <GoLocation />
+                  </div>
+                  <div className="touch-info">
+                    <h3>مكتبنا الرئيسي</h3>
+                    <span>123 Street, New York, USA</span>
+                  </div>
+                </div>
+                <div className="touch-left">
+                  <div className="touch-icon">
+                    <BsTelephoneInbound />
+                  </div>
+                  <div className="touch-info">
+                    <h3>أتصل بنا للمساعدة</h3>
+                    <span>+012 345 6789</span>
+                  </div>
+                </div>
+                <div className="touch-left">
+                  <div className="touch-icon">
+                    <HiOutlineMail />
+                  </div>
+                  <div className="touch-info">
+                    <h3>البريد الإلكتروني الخاص بنا</h3>
+                    <span>info@example.com</span>
+                  </div>
+                </div>
               </div>
-              <div className="touch-info">
-                <h3>مكتبنا الرئيسي</h3>
-                <span>123 Street, New York, USA</span>
-              </div>
-            </div>
-            <div className="touch-left">
-              <div className="touch-icon">
-                <BsTelephoneInbound />
-              </div>
-              <div className="touch-info">
-                <h3>أتصل بنا للمساعدة</h3>
-                <span>+012 345 6789</span>
-              </div>
-            </div>
-            <div className="touch-left">
-              <div className="touch-icon">
-                <HiOutlineMail />
-              </div>
-              <div className="touch-info">
-                <h3>البريد الإلكتروني الخاص بنا</h3>
-                <span>info@example.com</span>
-              </div>
-            </div>
-          </div>
-          <form className="touch-form " onSubmit={sendEmail}>
-            <input type="text" placeholder="الأسم" name="name" />
-            <input type="text" placeholder="البريد الألكتروني" name="email" />
-            <input type="text" placeholder="الموضوع" name="subject" />
-            <textarea
-              name="message"
-              id=""
-              cols="30"
-              rows="4"
-              placeholder="محتوي الرسالة"
-            ></textarea>
-            <input type="submit" className="btnMail" value={"أرسل الرسالة"} />
-          </form>
+            </Col>
+            <Col md={6}>
+              <form className="touch-form " onSubmit={sendEmail}>
+                <input type="text" placeholder="الأسم" name="name" />
+                <input
+                  type="text"
+                  placeholder="البريد الألكتروني"
+                  name="email"
+                />
+                <input type="text" placeholder="الموضوع" name="subject" />
+                <textarea
+                  name="message"
+                  id=""
+                  cols="30"
+                  rows="4"
+                  placeholder="محتوي الرسالة"
+                ></textarea>
+                <input
+                  type="submit"
+                  className="btnMail"
+                  value={"أرسل الرسالة"}
+                />
+              </form>
+            </Col>
+          </Row>
         </div>
       </div>
     </div>
